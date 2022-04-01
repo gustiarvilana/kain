15c4adc476e3a94dda4a8f981e65b1c71b685241
vilana31 <gustiar.ariefvilana31@gmail.com>
2022-04-01 14:27:47 +0700

    tambah string home dari komputer 2

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -14,7 +14,7 @@
                         </div>
                     @endif
 
-                    {{ __('You are logged in!') }}
+                    {{ __('You are logged in! ini edit komputer 2') }}
                 </div>
             </div>
         </div>
